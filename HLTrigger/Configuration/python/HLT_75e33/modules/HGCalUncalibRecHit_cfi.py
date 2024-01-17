c4e620dc30d4d9684faea1965795231ae6958a9b
AuroraPerego <aurora.perego@cern.ch>
2024-01-17 14:43:32 +0100

    changes to timing info for ticlv5 (#23)
    * Adding missing track variables to the dumper
    
    * add configurable local time for UncalibRechits
    
    * change the computation of the trackster time
    
    * Add track time variables to Dumper
    
    * ticlv5: compute candidate time at the vertex
    
    * fix warning in TICLCandidate validation
    
    * code checks and code format
    
    * replace InpuTag with std::string in hgcalValidator_cfi
    
    * ticlv5: producer for just merging the two clue3d collections
    
    * ticlv5: customise dumper for ticlv5
    
    * ticlv5: fix TICLCandidate constructor
    
    * ticlv5: add boundary time and change simTICLCandidate time
    
    * ticlv5: remove track quality and fix TICLCandidateProducer
    
    * ticlv5: trackster time computation
    
    - add the possibiility to compute local trackster time in
      PatternReconition plugins
    - fix arguments order in assignPCA
    
    * ticlv5: set local time in customiseForTICLv5
    
    * code-format and code-checks
    
    * ticlv5: fix tracksters in input to candidates in TICLDumper
    
    * change 1 and 1. to 1.f
    
    * ticlv5: fix candidates creation
    
    ---------
    
    Co-authored-by: Felice Pantaleo <felice.pantaleo@cern.ch>
    Co-authored-by: Wahid Redjeb <wahid.redjeb.wr@gmail.com>

--- HLTrigger/Configuration/python/HLT_75e33/modules/HGCalUncalibRecHit_cfi.py
+++ HLTrigger/Configuration/python/HLT_75e33/modules/HGCalUncalibRecHit_cfi.py
@@ -1,8 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 from ..psets.hgcal_reco_constants_cfi import HGCAL_reco_constants as HGCAL_reco_constants
+from RecoLocalCalo.HGCalRecProducers.HGCalUncalibRecHitProducer_cfi import HGCalUncalibRecHitProducer
 
-
-HGCalUncalibRecHit = cms.EDProducer("HGCalUncalibRecHitProducer",
+HGCalUncalibRecHit = HGCalUncalibRecHitProducer.clone(
     HGCEEConfig = cms.PSet(
         adcNbits = cms.uint32(10),
         adcSaturation = cms.double(100),
@@ -14,8 +14,6 @@ HGCalUncalibRecHit = cms.EDProducer("HGCalUncalibRecHitProducer",
         toaLSB_ns = cms.double(0.0244),
         tofDelay = cms.double(-9)
     ),
-    HGCEEdigiCollection = cms.InputTag("hgcalDigis","EE"),
-    HGCEEhitCollection = cms.string('HGCEEUncalibRecHits'),
     HGCHEBConfig = cms.PSet(
         adcNbits = cms.uint32(10),
         adcSaturation = cms.double(68.75),
@@ -27,8 +25,6 @@ HGCalUncalibRecHit = cms.EDProducer("HGCalUncalibRecHitProducer",
         toaLSB_ns = cms.double(0.0244),
         tofDelay = cms.double(-14)
     ),
-    HGCHEBdigiCollection = cms.InputTag("hgcalDigis","HEback"),
-    HGCHEBhitCollection = cms.string('HGCHEBUncalibRecHits'),
     HGCHEFConfig = cms.PSet(
         adcNbits = cms.uint32(10),
         adcSaturation = cms.double(100),
@@ -40,8 +36,6 @@ HGCalUncalibRecHit = cms.EDProducer("HGCalUncalibRecHitProducer",
         toaLSB_ns = cms.double(0.0244),
         tofDelay = cms.double(-11)
     ),
-    HGCHEFdigiCollection = cms.InputTag("hgcalDigis","HEfront"),
-    HGCHEFhitCollection = cms.string('HGCHEFUncalibRecHits'),
     HGCHFNoseConfig = cms.PSet(
         adcNbits = cms.uint32(10),
         adcSaturation = cms.double(100),
@@ -50,10 +44,7 @@ HGCalUncalibRecHit = cms.EDProducer("HGCalUncalibRecHitProducer",
         tdcNbits = cms.uint32(12),
         tdcOnset = cms.double(60),
         tdcSaturation = cms.double(10000),
-        toaLSB_ns = cms.double(0.0244),
-        tofDelay = cms.double(-33)
-    ),
-    HGCHFNosedigiCollection = cms.InputTag("hfnoseDigis","HFNose"),
-    HGCHFNosehitCollection = cms.string('HGCHFNoseUncalibRecHits'),
-    algo = cms.string('HGCalUncalibRecHitWorkerWeights')
+        tofDelay = cms.double(-33),
+        toaLSB_ns = cms.double(0.0244)
+    )
 )
