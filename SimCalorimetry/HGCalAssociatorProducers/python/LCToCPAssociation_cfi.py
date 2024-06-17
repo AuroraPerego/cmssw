f3ca2a00ac2eed8d98db57e3e3e7279b5bef59d1
AuroraPerego <aurora.perego@cern.ch>
2024-06-17 12:11:27 +0200

    enable CP from PU

--- SimCalorimetry/HGCalAssociatorProducers/python/LCToCPAssociation_cfi.py
+++ SimCalorimetry/HGCalAssociatorProducers/python/LCToCPAssociation_cfi.py
@@ -3,7 +3,8 @@ import FWCore.ParameterSet.Config as cms
 layerClusterCaloParticleAssociation = cms.EDProducer("LCToCPAssociatorEDProducer",
     associator = cms.InputTag('lcAssocByEnergyScoreProducer'),
     label_cp = cms.InputTag("mix","MergedCaloTruth"),
-    label_lc = cms.InputTag("hgcalMergeLayerClusters")
+    label_lc = cms.InputTag("hgcalMergeLayerClusters"),
+    hardScatterOnly = cms.bool(False)
 )
 
 from Configuration.ProcessModifiers.premix_stage2_cff import premix_stage2
