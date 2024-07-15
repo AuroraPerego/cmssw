2175cd7ccbaa6f652f9287445ee71496d82f3fcb
AuroraPerego <aurora.perego@cern.ch>
2024-07-15 14:25:46 +0200

    add support for oneAPI in AlpakaInterface

--- HeterogeneousCore/AlpakaInterface/interface/Backend.h
+++ HeterogeneousCore/AlpakaInterface/interface/Backend.h
@@ -5,7 +5,15 @@
 
 namespace cms::alpakatools {
   // Enumeration whose value EDModules can put in the event
-  enum class Backend : unsigned short { SerialSync = 0, CudaAsync = 1, ROCmAsync = 2, TbbAsync = 3, size };
+  enum class Backend : unsigned short {
+    SerialSync = 0,
+    CudaAsync = 1,
+    ROCmAsync = 2,
+    TbbAsync = 3,
+    SYCLCpuAsync = 4,
+    SYCLIntelGpuAsync = 5,
+    size
+  };
 
   Backend toBackend(std::string_view name);
   std::string_view toString(Backend backend);
