a12bdef3dd89297c58b5932682874921163088ea
AuroraPerego <aurora.perego@cern.ch>
2025-05-08 14:15:26 +0200

    use track energy

--- RecoHGCal/TICL/plugins/GeneralInterpretationAlgo.h
+++ RecoHGCal/TICL/plugins/GeneralInterpretationAlgo.h
@@ -38,7 +38,8 @@ namespace ticl {
                               float zVal,
                               std::array<TICLLayerTile, 2> &tracksterTiles);
 
-    void findTrackstersInWindow(const MultiVectorManager<Trackster> &tracksters,
+    void findTrackstersInWindow(const std::vector<reco::Track> &tracks,
+                                const MultiVectorManager<Trackster> &tracksters,
                                 const std::vector<std::pair<Vector, unsigned>> &seedingCollection,
                                 const std::array<TICLLayerTile, 2> &tracksterTiles,
                                 const std::vector<Vector> &tracksterPropPoints,
